04f06b9400bd2575c512b267755586bf52dbbdbf
IT21042010 <87034519+Redsniper99@users.noreply.github.com>
2023-01-22 18:49:23 +0530

    Initial commit

---
+++ React_Responsive_Website_1/src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Billing from "./Billing";
+import Footer from "./Footer";  
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+
+export{
+    Navbar,
+    Billing,
+    CardDeal,
+    Business,
+    CTA,
+    Footer,
+    Hero,
+    Testimonials,
+    Clients,
+    Stats
+};
